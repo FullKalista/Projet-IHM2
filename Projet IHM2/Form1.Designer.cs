d9a43e3e312898bbfb185e6abc95cbad16500e44
Laureline <57068799+FullKalista@users.noreply.github.com>
2022-05-02 09:23:13 +0200

    ajout de la solution

---
+++ Projet IHM2/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Projet_IHM2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label1 = new System.Windows.Forms.Label();
+            this.listBoxMatieres = new System.Windows.Forms.ListBox();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtAjoutLibelle = new System.Windows.Forms.TextBox();
+            this.txtAjoutCode = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.txtModifLibelle = new System.Windows.Forms.TextBox();
+            this.txtModifCode = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.GroupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtFiltreCode = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.GroupBox1.SuspendLayout();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.GroupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(558, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(183, 24);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Gestion des matières";
+            // 
+            // listBoxMatieres
+            // 
+            this.listBoxMatieres.FormattingEnabled = true;
+            this.listBoxMatieres.ItemHeight = 16;
+            this.listBoxMatieres.Location = new System.Drawing.Point(12, 9);
+            this.listBoxMatieres.Name = "listBoxMatieres";
+            this.listBoxMatieres.Size = new System.Drawing.Size(344, 660);
+            this.listBoxMatieres.TabIndex = 1;
+            this.listBoxMatieres.SelectedIndexChanged += new System.EventHandler(this.listBoxMatieres_SelectedIndexChanged);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.btnAjouter);
+            this.GroupBox1.Controls.Add(this.txtAjoutLibelle);
+            this.GroupBox1.Controls.Add(this.txtAjoutCode);
+            this.GroupBox1.Controls.Add(this.Label3);
+            this.GroupBox1.Controls.Add(this.Label2);
+            this.GroupBox1.Location = new System.Drawing.Point(432, 55);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(509, 166);
+            this.GroupBox1.TabIndex = 2;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Ajouter";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(119, 118);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(91, 26);
+            this.btnAjouter.TabIndex = 4;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // txtAjoutLibelle
+            // 
+            this.txtAjoutLibelle.Location = new System.Drawing.Point(119, 77);
+            this.txtAjoutLibelle.MaxLength = 50;
+            this.txtAjoutLibelle.Name = "txtAjoutLibelle";
+            this.txtAjoutLibelle.Size = new System.Drawing.Size(295, 22);
+            this.txtAjoutLibelle.TabIndex = 3;
+            // 
+            // txtAjoutCode
+            // 
+            this.txtAjoutCode.Location = new System.Drawing.Point(119, 30);
+            this.txtAjoutCode.MaxLength = 5;
+            this.txtAjoutCode.Name = "txtAjoutCode";
+            this.txtAjoutCode.Size = new System.Drawing.Size(91, 22);
+            this.txtAjoutCode.TabIndex = 2;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(56, 82);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(57, 17);
+            this.Label3.TabIndex = 1;
+            this.Label3.Text = "Libellé :";
+            // 
+            // Label2
+            // 
+            this.Label2.Location = new System.Drawing.Point(56, 30);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(100, 23);
+            this.Label2.TabIndex = 0;
+            this.Label2.Text = "Code : ";
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.btnModifier);
+            this.GroupBox2.Controls.Add(this.txtModifLibelle);
+            this.GroupBox2.Controls.Add(this.txtModifCode);
+            this.GroupBox2.Controls.Add(this.Label5);
+            this.GroupBox2.Controls.Add(this.Label4);
+            this.GroupBox2.Location = new System.Drawing.Point(432, 247);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(509, 166);
+            this.GroupBox2.TabIndex = 3;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Modifier";
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(119, 118);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(91, 26);
+            this.btnModifier.TabIndex = 4;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // txtModifLibelle
+            // 
+            this.txtModifLibelle.Location = new System.Drawing.Point(119, 79);
+            this.txtModifLibelle.MaxLength = 50;
+            this.txtModifLibelle.Name = "txtModifLibelle";
+            this.txtModifLibelle.Size = new System.Drawing.Size(295, 22);
+            this.txtModifLibelle.TabIndex = 3;
+            // 
+            // txtModifCode
+            // 
+            this.txtModifCode.Location = new System.Drawing.Point(120, 30);
+            this.txtModifCode.MaxLength = 5;
+            this.txtModifCode.Name = "txtModifCode";
+            this.txtModifCode.Size = new System.Drawing.Size(91, 22);
+            this.txtModifCode.TabIndex = 2;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(56, 82);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(57, 17);
+            this.Label5.TabIndex = 1;
+            this.Label5.Text = "Libellé :";
+            // 
+            // Label4
+            // 
+            this.Label4.Location = new System.Drawing.Point(56, 30);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(100, 23);
+            this.Label4.TabIndex = 0;
+            this.Label4.Text = "Code : ";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.btnSupprimer);
+            this.GroupBox3.Location = new System.Drawing.Point(432, 434);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Size = new System.Drawing.Size(509, 81);
+            this.GroupBox3.TabIndex = 5;
+            this.GroupBox3.TabStop = false;
+            this.GroupBox3.Text = "Supprimer l\'enregistrement sélectionné";
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(120, 37);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(91, 26);
+            this.btnSupprimer.TabIndex = 4;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // GroupBox4
+            // 
+            this.GroupBox4.Controls.Add(this.txtFiltreCode);
+            this.GroupBox4.Controls.Add(this.Label6);
+            this.GroupBox4.Location = new System.Drawing.Point(432, 537);
+            this.GroupBox4.Name = "GroupBox4";
+            this.GroupBox4.Size = new System.Drawing.Size(509, 81);
+            this.GroupBox4.TabIndex = 6;
+            this.GroupBox4.TabStop = false;
+            this.GroupBox4.Text = "Filtrer par le début du code";
+            // 
+            // txtFiltreCode
+            // 
+            this.txtFiltreCode.Location = new System.Drawing.Point(119, 40);
+            this.txtFiltreCode.MaxLength = 5;
+            this.txtFiltreCode.Name = "txtFiltreCode";
+            this.txtFiltreCode.Size = new System.Drawing.Size(91, 22);
+            this.txtFiltreCode.TabIndex = 4;
+            this.txtFiltreCode.TextChanged += new System.EventHandler(this.txtFiltreCode_TextChanged);
+            // 
+            // Label6
+            // 
+            this.Label6.Location = new System.Drawing.Point(56, 39);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(100, 23);
+            this.Label6.TabIndex = 3;
+            this.Label6.Text = "Code : ";
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(876, 669);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(91, 26);
+            this.btnQuitter.TabIndex = 5;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 707);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.GroupBox4);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.listBoxMatieres);
+            this.Controls.Add(this.Label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion des matières";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox4.ResumeLayout(false);
+            this.GroupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.ListBox listBoxMatieres;
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox txtAjoutLibelle;
+        private System.Windows.Forms.TextBox txtAjoutCode;
+        private System.Windows.Forms.GroupBox GroupBox2;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.TextBox txtModifLibelle;
+        private System.Windows.Forms.TextBox txtModifCode;
+        private System.Windows.Forms.Label Label5;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.GroupBox GroupBox3;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.GroupBox GroupBox4;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.TextBox txtFiltreCode;
+        private System.Windows.Forms.Label Label6;
+    }
+}
+
---
+++ Projet IHM2/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Projet_IHM2
+{
+    public partial class Form1 : Form
+    {
+        // variables globales
+        static OleDbConnection cnx;     // l'objet de connexion à la base de données
+
+        // le constructeur de la class Form1 (on n'y touche pas !)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // cette fonction s'exécute lors de l'évènement Load du formulaire qui indique la fin du chargement en mémoire
+        // c'est une des premières fonctions à s'exécuter, on y place donc des traitements d'initialisation
+        // comme la connexion à une base de données
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // chaîne de connexion à une base de données Access (Jet est le moteur d'Access)
+            // Data Source ne précise pas de chemin
+            // ce qui signifie que la base Lycée2012 accdb est placée dans le dossier d'exécution (bin/Debug)
+            // ATTENTION : bien respecter la casse et ne pas ajouter ou retirer d'espace
+            const String CHAINE_CNX = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Lycee2012.accdb;";
+            String msg;
+
+            // un bloc Try (essayer) sert à placer des instructions qui risquent de "planter"
+            // (par exemple : tentative de connexion à une base de données absente ou défectueuse)
+            // en cas d'erreur d'exécution, le programme se déroute automatiquement vers le bloc Catch
+            try
+            {
+                // Connexion à la base données
+                cnx = new OleDbConnection(CHAINE_CNX);
+                cnx.Open();
+
+                // affichage des données dans le contrôle listBoxMatieres
+                afficherDonnees();
+            }
+            catch (Exception ex)
+            {
+                // un bloc Catch permet de "reprendre la main" lors d'une erreur d'exécution
+                // l'objet ex (de la classe Exception) représente l'erreur
+                // sa propriété ex.Message done le libellé de l'erreur
+                msg = "Problème de connexion à la base de données \r\r" + ex.Message;
+                MessageBox.Show(msg, "Problème", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        // cette fonction s'exécute lors de l'évènement FormClosing du formulaire qui précède la fermeture du formulaire
+        // c'est une des dernières fonctions à s'exécuter, on y place donc des traitements terminaux
+        // comme la déconnexion d'une base de données
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // fermeture de la connexion (si elle est ouverte)
+            if (cnx.State == ConnectionState.Open)
+            {
+                cnx.Close();
+            }
+
+            // suppression de l'objet cnx
+            cnx = null;
+        }
+
+        // cette fonction s'exécute lors d'un clic sur le bouton btnQuitter et ferme le formulaire
+        // ce qui déclenche l'évènement FormClosing, et donc la déconnexion de la base
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // cette procédure affiche les matières dans le contrôle listBoMatieres
+        // elle utilise un objet de la classe DataTable pour stocker le jeu de données (jdd)
+        // obtenu avec une requête SQL de type SELECT
+        private void afficherDonnees()
+        {
+            // requête SQL de sélection des données
+            String req;
+            req = "SELECT * FROM Matieres WHERE CodeMatiere LIKE '";
+            req += this.txtFiltreCode.Text + "%' ORDER BY CodeMatiere";
+            OleDbDataAdapter cmd;                           // la source de données (correspond à la connexion et à la requête SELECT)
+            DataTable jdd;                                  // les données résultant de la requête SELECT(jdd = Jeu De Données)
+            String uneLigne;                                // une ligne de texte à ajouter à la collection listBoxMatieres.Items
+
+            this.listBoxMatieres.Items.Clear();             // efface le contenu de la collection listBoxMatieres
+            cmd = new OleDbDataAdapter(req, cnx);           // préparation de la source de données
+            jdd = new DataTable("Matières");                // on donne un nom significatif à l'objet DataTable
+            cmd.Fill(jdd);                                  // exécution de la requête et récupération des données
+
+            // boucle de remplissage de la collection listBoxMatieres.Items
+            // la variable unEnreg représente l'enregistrement courant de jdd
+            foreach (DataRow unEnreg in jdd.Rows)           // pour un enregistrement de l'objet DataTable
+            {
+                // traitement de l'enregistrement : ajout d'une ligne de texte dans la collection d'éléements de listBoxMatieres
+                // unEnreg["CodeMatiere"] désigne la colonne CodeMatiere dans l'enregistrement courant du unEnreg
+                // PadRight complète le code matière avec des espaces à droite pour le mettre à une longueur de 5 caractères
+                uneLigne = Convert.ToString(unEnreg["CodeMatiere"]).PadRight(3, ' ');
+                uneLigne = uneLigne + " - " + Convert.ToString(unEnreg["LibelleMatiere"]);
+                this.listBoxMatieres.Items.Add(uneLigne);
+            }
+
+            // suppression du jeu de données
+            jdd = null;
+            cmd = null;
+
+            // sélection automatique de la première ligne du contrôle listBox
+            if(this.listBoxMatieres.Items.Count > 0)
+            {
+                this.listBoxMatieres.SelectedIndex = 0;
+            }
+        }
+
+        // cette fonction s'exécute lors d'un clic sur le bouton btnAjouter
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            String msg;
+            String req;             // requête SQL du type INSERT
+            OleDbCommand cmd;       // la commande à exécuter
+            if (this.txtAjoutCode.Text == "" || this.txtAjoutLibelle.Text == "")
+            {
+                msg = "Les données doivent être saisies";
+                MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                // construction de la requête d'insertion
+                req = "INSERT INTO Matieres(CodeMatiere, LibelleMatiere) VALUES";
+                req += "('" + this.txtAjoutCode.Text.ToUpper() + "'";
+                req += ",'" + this.txtAjoutLibelle.Text.Replace("'", "''") + "')";
+
+                try
+                {
+                    cmd = new OleDbCommand(req, cnx);               // prépare la requête
+                    cmd.ExecuteNonQuery();                          // exécute la requête
+                    afficherDonnees();                              // affiche les données (avec une ligne en plus et un nouveau tri)
+                }
+                catch (Exception ex)
+                {
+                    // une erreur d'exécution s'est produite
+                    msg = ex.Message + "\r" + req;
+                    MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        // cette fonction s'exécute lorsqu'on sélectionne une nouvelle ligne dans le contrôle listBoxMatieres
+        private void listBoxMatieres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.txtModifCode.Text = this.listBoxMatieres.Text.Substring(0, 5);
+            this.txtModifLibelle.Text = this.listBoxMatieres.Text.Substring(8, this.listBoxMatieres.Text.Length - 8);
+        }
+
+        // cette fonction s'exécute lors d'un clic sur le bouton btnModifier
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            String msg;
+            String req;             // requête SQL du type UPDATE
+            OleDbCommand cmd;       // la commande à exécuter
+
+            if (this.txtModifCode.Text == "" || this.txtModifLibelle.Text == "")
+            {
+                msg = "Les données doivent être saisies";
+                MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                // construction de la requête de mise à jour
+                req = "UPDATE Matieres";
+                req += " set LibelleMatiere = '" + this.txtModifLibelle.Text.Replace("'", "''") + "'";
+                req += " WHERE CodeMatiere = '" + this.txtModifCode.Text + "'";
+                try
+                {
+                    cmd = new OleDbCommand(req, cnx);           // prépare la requête
+                    cmd.ExecuteNonQuery();                      // exécute la requête 
+                    afficherDonnees();                          // affiche les données ( avec une ligne de plus et un nouveau tri)
+                }
+                catch (Exception ex)
+                {
+                    // une erreur d'exécution s'est produite
+                    msg = ex.Message + "\r" + req;
+                    MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        // cette fonction s'exécute lors d'un clic sur le bouton btnSupprimer
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            String msg;
+            String req;             // requête SQL du type UPDATE
+            OleDbCommand cmd;       // la commande à exécuter
+
+            if (this.listBoxMatieres.Text == "")
+            {
+                msg = "Il faut sélectionner un enregistrement";
+                MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                // construction de la requête de suppression
+                req = "DELETE FROM Matieres WHERE CodeMatiere = " + this.listBoxMatieres.Text.Substring(0, 5).Trim() + "'";
+                req += " set LibelleMatiere = '" + this.txtModifLibelle.Text + "'";
+                req += " WHERE CodeMatiere = '" + this.txtModifCode.Text + "'";
+                try
+                {
+                    cmd = new OleDbCommand(req, cnx);           // prépare la requête
+                    cmd.ExecuteNonQuery();                      // exécute la requête 
+                    afficherDonnees();                          // affiche les données ( avec une ligne de plus et un nouveau tri)
+                }
+                catch (Exception ex)
+                {
+                    // une erreur d'exécution s'est produite
+                    msg = ex.Message + "\r" + req;
+                    MessageBox.Show(msg, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        // cette fonction s'exécute à chaque changement du contenu de la zone de txtFiltreCode
+        // elle réaffiche les données en tenant compte du nouveau filtre saisi
+        private void txtFiltreCode_TextChanged(object sender, EventArgs e)
+        {
+            afficherDonnees();
+        }
+
+    }
+}
